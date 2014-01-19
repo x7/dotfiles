a2e418d5c0e703e22e19bb3fec5b4424829960f6
Alex <alexmail87@gmail.com>
2014-01-19 17:29:36 -0500

    Consolidate path.zsh

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,11 @@
-export PATH="$HOME/.dotfiles/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
+export PATH="$HOME/.dotfiles/bin:$PATH"
+
+export PATH="$HOME/.rbenv/bin:$PATH"
+export PATH="$HOME/.rbenv/shims:$PATH"
+
+export PATH="$ZSH/bin:$PATH"
+
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
